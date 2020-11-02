a4dd67b7773e4f761268e1c073eb27e8c77d6f77
SimonN <65023744+Simon994@users.noreply.github.com>
2020-11-02 18:58:36 +0000

    initial creation of react-app and start of Navbar

---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Navbar extends React.Component {
+
+  render(){
+    return (
+      <nav>
+        <ul>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Skills</li>
+          <li>Other</li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Navbar
