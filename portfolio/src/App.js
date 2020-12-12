0d88134a7b647095694a48b458fe15900ac2d3dd
SimonN <65023744+Simon994@users.noreply.github.com>
2020-12-12 17:33:38 +0000

    Remove fade between views. Lint SCSS.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import { CSSTransition, TransitionGroup } from 'react-transition-group'
+// import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 import Navbar from './components/common/Navbar'
 import Home from './components/infos/Home'
@@ -13,18 +13,10 @@ class App extends React.Component {
       <>
         <Navbar />
         <Route render={({ location }) => (
-          <TransitionGroup>
-            <CSSTransition
-              key={location.key}
-              timeout={700}
-              classNames='fade'
-            >
-              <Switch location={location}>
-                <Route exact path='/' component={Home}></Route>
-                <Route path='/skills' component={Skills}></Route>
-              </Switch>
-            </CSSTransition>
-          </TransitionGroup>
+          <Switch location={location}>
+            <Route exact path='/' component={Home}></Route>
+            <Route path='/skills' component={Skills}></Route>
+          </Switch>
         )} />
       </>
     )
