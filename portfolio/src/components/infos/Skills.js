9f282161894924910ef77059c4e8ab0e96eb64a9
SimonN <65023744+Simon994@users.noreply.github.com>
2020-12-18 21:07:03 +0000

    WIP. Add React icons for Skills

--- portfolio/src/components/infos/Skills.js
+++ portfolio/src/components/infos/Skills.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { DiGit } from 'react-icons/di'
 
 const Skills = () => {
   return (
-    <div className="glitch content-border" data-text="Skills">Skills</div>
+    <div className="glitch content-border" data-text="Skills">Skills
+      <div>
+        <DiGit className='icon'/>
+      </div>
+    </div>
   )
 }
 
