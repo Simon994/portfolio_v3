63beb80ba1132956c50e431b666fe436abe8f074
SimonN <65023744+Simon994@users.noreply.github.com>
2020-12-23 19:23:46 +0000

    WIP. Project card animations

---
+++ portfolio/src/components/infos/ProjectCard.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const ProjectCard = (props) => {
+  
+  const {
+    githubLink,
+    imgLink,
+    altText,
+    titleText,
+    infoText,
+    liveLink
+  } = props
+
+  return (
+    <div className='project-card card-border'>
+      <a href={githubLink}>
+        <img src={imgLink} alt={altText}/>
+      </a> 
+      <div >
+        <div >
+          <h5>{titleText}</h5>
+          <p>{infoText}<br/>
+            <a href={githubLink}>Github repository</a><br/>
+            <a href={liveLink}>Live version</a>
+          </p>
+        </div>
+      </div> 
+    </div>
+  )
+}
+
+export default ProjectCard
--- portfolio/src/components/infos/Projects.js
+++ portfolio/src/components/infos/Projects.js
@@ -3,39 +3,30 @@ import React from 'react'
 
 import redshirtsRun from '../../styles/screenshots/Redshirts_run_screenshot.png'
 import newsFeed from '../../styles/screenshots/News_Feed_V2Global.png'
+import ProjectCard from './ProjectCard'
 
 const Projects = () => {
   return (
-    <div className='projects-outer-container'>
+    <div className='projects-outer-container fade-in'>
       <div className='projects-cards-container'>
-        <div className='project-card'>
-          <a href="https://github.com/Simon994/sei-project-1">
-            <img src={redshirtsRun} alt="Redshirt's run demo"/>
-          </a> 
-          <div >
-            <div >
-              <h5>Redshirt's run</h5>
-              <p>A grid-based game built with JavaScript, HTML5, CSS3.<br/>
-                <a href="https://github.com/Simon994/sei-project-1">Github repository</a><br/>
-                <a href="https://simon994.github.io/sei-project-1/">Live version</a>
-              </p>
-            </div>
-          </div> 
-        </div>
-        <div className='project-card'>
-          <a href="https://github.com/Simon994/news-feed-v2Global">
-            <img src={newsFeed} alt="NewsFeed demo"/>
-          </a> 
-          <div >
-            <div >
-              <h5>News Feed v2Global</h5>
-              <p>A React app that consumes API data from GNews.<br/>
-                <a href="https://github.com/Simon994/news-feed-v2Global">Github repository</a><br/>
-                <a href="https://the-news-feed-v2-global.netlify.app">Live version</a>
-              </p>
-            </div>
-          </div> 
-        </div>
+        <ProjectCard 
+          className='project-card card-border'
+          githubLink='https://github.com/Simon994/sei-project-1'
+          imgLink={redshirtsRun}
+          altText="Redshirt's run demo"
+          titleText="Redshirt's run"
+          infoText='A grid-based game built with JavaScript, HTML5, CSS3.'
+          liveLink='https://simon994.github.io/sei-project-1/'
+        />
+        <ProjectCard 
+          className='project-card card-border'
+          githubLink='https://github.com/Simon994/news-feed-v2Global'
+          imgLink={newsFeed}
+          altText="NewsFeed demo"
+          titleText="News Feed v2Global"
+          infoText='A React app that consumes API data from GNews.'
+          liveLink='https://the-news-feed-v2-global.netlify.app'
+        />
         <div className='project-card'>
 
         </div>
