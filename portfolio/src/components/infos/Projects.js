2928ec05c43f0ba2bd9ee56d84dbc4e5907870d5
SimonN <65023744+Simon994@users.noreply.github.com>
2021-01-01 19:33:28 +0000

    Update Projects with images and text

--- portfolio/src/components/infos/Projects.js
+++ portfolio/src/components/infos/Projects.js
@@ -1,10 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 
-// import redshirtsRun from '../../styles/screenshots/Redshirts_run_screenshot.png'
-// import newsFeed from '../../styles/screenshots/News_Feed_V2Global.png'
-// import ProjectCard from './ProjectCard'
-
 const Projects = () => {
   return (
     <>
@@ -13,56 +9,47 @@ const Projects = () => {
         <div className="project-img1-glitch"></div>
         <div className="project-img1-glitch"></div>
         <div className="project-img1-glitch"></div>
-        <div className="project-title">
-          <span className="border" style={{ display: 'inline-block', color: 'black' }}>JavaScript, HTML5, CSS3</span>
-        </div>
       </div>
 
       <section className="project-blurb section-light">
-        <h3>Redshirt's Run</h3>
-        <p>A grid-based game, inspired by Pacman and Star Trek, built with vanilla JavaScript, HTML5 and CSS3.</p>
-        <p>Built solo in a week, three weeks into my Software Engineering Immersive at General Assembly</p>
-        <p>Play the live version <a href="https://simon994.github.io/sei-project-1/">here</a></p>
-        <p>See the GitHub repo <a href="https://github.com/Simon994/sei-project-1">here</a></p>
+        <h3>499px (RAWshot)</h3>
+        <p>A full-stack app, with multiple relationships and CRUD functionality, cloning key parts of the photo-sharing website 500px</p>
+        <p>Technologies include: Django, Django REST framework, React, PostgreSQL, SASS, Axios and Semantic UI React</p>
+        <p>Built solo in 8 days</p>
+        <p><a href="https://rawshot-499px.herokuapp.com/">Live version</a>  |  <a href="https://github.com/Simon994/RAWShot">GitHub repo</a> </p>
+        <p></p>
       </section>
 
-
-      <div className="project-img2-container">
-        <div className="project-title">
-          <span className="border" style={{ display: 'inline-block', color: 'black' }}>React.js, consuming 3rd party API data</span>
-        </div>
-      </div>
-
-      <section className="project-blurb section-dark">
-        <h3>News Feed</h3>
-        <p>A grid-based game, inspired by Pacman and Star Trek, built with vanilla JavaScript, HTML5 and CSS3.</p>
-        <p>Built solo in a week, three weeks into my Software Engineering Immersive at General Assembly</p>
-        <p>Play the live version <a href="https://simon994.github.io/sei-project-1/">here</a></p>
-        <p>See the GitHub repo <a href="https://github.com/Simon994/sei-project-1">here</a></p>
-      </section>
-
-
-      <div className="project-img3-container">
-        <div className="project-title">
-          <span className="border" style={{ display: 'inline-block', color: 'black' }}>Full-stack MERN</span>
-        </div>
-      </div>
+      <div className="project-img2-container"></div>
 
       <section className="project-blurb section-dark">
         <h3>Discover Montréal</h3>
-        <p>A grid-based game, inspired by Pacman and Star Trek, built with vanilla JavaScript, HTML5 and CSS3.</p>
-        <p>Built solo in a week, three weeks into my Software Engineering Immersive at General Assembly</p>
-        <p>Play the live version <a href="https://simon994.github.io/sei-project-1/">here</a></p>
-        <p>See the GitHub repo <a href="https://github.com/Simon994/sei-project-1">here</a></p>
+        <p>Full-stack MERN app with multiple relationships and CRUD functionality</p>
+        <p>Users can share, rate and comment on attractions around Montréal</p>
+        <p>Built in 9 days as part of a 4-person team. My focus was split ca.80:20 between frontend and backend, respectively</p>
+        <p><a href="https://discover-montreal.herokuapp.com/">Live version</a>  |  <a href="https://github.com/Simon994/sei-project-3">GitHub repo</a> </p>
+      </section>
+
+      <div className="project-img3-container"></div>
+
+      <section className="project-blurb section-dark">
+        <h3>News Feed <span style={{ fontWeight: '400' }}>and</span> News Feed v2Global</h3>
+        <p>A React app that consumes 3rd party API data from News API</p>
+        <p>My focus included building the top-stories section and other components, styling, bug-fixing, and contributing to overall project planning</p>
+        <p>Built in 3 days, as part of a two-person collaborative project</p>
+        <p><a href="https://the-news-feed-v2-global.netlify.app/">Live version (v2Global)</a>  |  <a href="https://github.com/Simon994/news-feed-v2Global">GitHub repo (v2Global)</a>  |  <a href="https://github.com/Simon994/news-feed">GitHub repo (News Feed)</a>
+        </p>
       </section>
 
 
-      <div className="project-img1">
-        <div className="project-title">
-          <span className="border" style={{ display: 'inline-block', color: 'black' }}>JavaScript, HTML5, CSS3</span>
-        </div>
-      </div>
+      <div className="project-img4-container"></div>
 
+      <section className="project-blurb section-light">
+        <h3>Redshirt's Run</h3>
+        <p>A grid-based game, inspired by Pacman and Star Trek, built with vanilla JavaScript, HTML5 and CSS3</p>
+        <p>Built solo in a week, three weeks into my Software Engineering Immersive at General Assembly</p>
+        <p><a href="https://simon994.github.io/sei-project-1/">Live version</a>  |  <a href="https://github.com/Simon994/sei-project-1">GitHub repo</a> </p>
+      </section>
 
     </>
 
