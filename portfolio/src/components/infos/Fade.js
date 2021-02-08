ec3e5cec201c8867610afa35c4acec5f49a78285
SimonN <65023744+Simon994@users.noreply.github.com>
2021-02-08 13:58:12 +0000

    Add fade effect in project text

---
+++ portfolio/src/components/infos/Fade.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function FadeIn(props) {
+  const domRef = React.useRef()
+  const [isVisible, setVisible] = React.useState(false)
+  
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => setVisible(entry.isIntersecting))
+    })
+    observer.observe(domRef.current)
+  }, [])
+  return (
+    <div
+      className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
+      ref={domRef}
+    >
+      {props.children}
+    </div>
+  );
+}
