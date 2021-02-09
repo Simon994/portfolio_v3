a2a65eb5342b9182d53b4ba5da3bfc8b9924fff2
SimonN <65023744+Simon994@users.noreply.github.com>
2021-02-09 11:32:34 +0000

    Remove cruft

--- portfolio/src/components/infos/Fade.js
+++ portfolio/src/components/infos/Fade.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
+// const observerOptions = {
+//   root: null,
+//   rootMargin: '0px',
+//   threashold: 0
+// }
+
 export default function FadeIn(props) {
   const domRef = React.useRef()
   const [isVisible, setVisible] = React.useState(false)
@@ -14,9 +20,8 @@ export default function FadeIn(props) {
   )
 
   React.useEffect(() => {
-    const { current: currentObserver } = observer
-    currentObserver.observe(domRef.current)
-    return () => currentObserver.disconnect()
+    observer.observe(domRef.current)
+    return () => observer.disconnect()
   })
 
   return (
