4c5696d5d68a4b08bef5d33a671d5c7a9f146b1b
SimonN <65023744+Simon994@users.noreply.github.com>
2021-02-09 11:16:29 +0000

    Refactor FadeIn and update to include disconnect

--- portfolio/src/components/infos/Fade.js
+++ portfolio/src/components/infos/Fade.js
@@ -1,15 +1,30 @@
 import React from 'react'
 
+// const observerOptions = {
+//   root: null,
+//   rootMargin: '0px',
+//   threashold: 0
+// }
+
 export default function FadeIn(props) {
   const domRef = React.useRef()
   const [isVisible, setVisible] = React.useState(false)
   
+  const observer = new IntersectionObserver(
+    entries => {
+      entries.forEach(function(entry) {
+        setVisible(entry.isIntersecting)
+        } 
+      )
+    }
+  )
+
   React.useEffect(() => {
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => setVisible(entry.isIntersecting))
-    })
-    observer.observe(domRef.current)
-  }, [])
+    const { current: currentObserver } = observer
+    currentObserver.observe(domRef.current)
+    return () => currentObserver.disconnect()
+  })
+
   return (
     <div
       className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
