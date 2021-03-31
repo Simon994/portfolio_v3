3a00baa62d5163102089b9812d9fc893d4d3d103
SimonN <65023744+Simon994@users.noreply.github.com>
2021-03-31 09:58:47 +0100

    Trim commit message. Refactor functions for GitHub api calls

--- portfolio/src/components/infos/GitHubActivity.js
+++ portfolio/src/components/infos/GitHubActivity.js
@@ -1,50 +1,37 @@
 import React from 'react'
-import { Octokit } from '@octokit/core'
 import parse from 'html-react-parser'
 
-function GitHubActivity() {
-  const [events, setEvents] = React.useState([])
-  const [readme, setReadme] = React.useState(null)
-  const octokit = new Octokit()
+import { getLatestGitHubEvent, getGitHubReadme } from '../../lib/api'
 
+function GitHubActivity() {
+  const [commitMessage, setCommitMessage] = React.useState(null)
+  const [readme, setReadme] = React.useState(null)
 
   React.useEffect(() => {
-
-    const eventsPerPage = 1
-
     async function getMostRecentEvents() {
       try {
-        const response = await octokit.request(
-          `/users/{username}/events/public?per_page=${eventsPerPage}`, { username: 'Simon994' }
-        )
+        const latestGitHubEvent = await getLatestGitHubEvent()
+        const repoName = latestGitHubEvent.data[0].repo.name
 
-        const repoName = response.data[0].repo.name
-        setEvents(response.data)
+        // Trim commit message to return only the first line
+        setCommitMessage(trimCommitMessage(latestGitHubEvent))
 
+        const readme = await getGitHubReadme(repoName)
+        setReadme(readme)
 
-        const readmeResponse = await octokit.request(
-          `/repos/${repoName}/readme`,
-          {
-            headers: {
-              accept: 'application/vnd.github.VERSION.html'
-            }
-          }
-        )
-        console.log('GOT README RESPONSE!!!!>>>>>', readmeResponse)
-        setReadme(readmeResponse.data)
       } catch (error) {
         console.error('SOMETHING WENT WRONG! :(', error)
       }
     }
-
     getMostRecentEvents()
   }, [])
 
   return (
     <>
       <div style={{ color: 'white' }}>
-        {events.length &&
-            <p>{events[0].payload.commits[0].message}</p>
+        <p>Latest commit</p>
+        {commitMessage &&
+            <p>{commitMessage}</p>
         }
         {
           readme &&
@@ -55,6 +42,11 @@ function GitHubActivity() {
       </div>
     </>
   )
+}
+
+const trimCommitMessage = (eventsResponse) => {
+  const messageText = eventsResponse.data[0].payload.commits[0].message
+  return messageText.split('\n')[0]
 }
 
 export default GitHubActivity
---
+++ portfolio/src/lib/api.js
@@ -0,0 +1,23 @@
+import { Octokit } from '@octokit/core'
+
+export const getLatestGitHubEvent = () =>{
+  const octokit = new Octokit()
+  const latestEvent = octokit.request(
+    '/users/{username}/events/public?per_page=1', { username: 'Simon994' }
+  )
+  return latestEvent
+}
+
+//Get README, as html, for specified repo
+export const getGitHubReadme = async (repoName) => {
+  const octokit = new Octokit()
+  const response = await octokit.request(
+    `/repos/${repoName}/readme`,
+    {
+      headers: {
+        accept: 'application/vnd.github.VERSION.html'
+      }
+    }
+  )
+  return response.data
+}
